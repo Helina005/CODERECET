fc5ec74702b87608a38b70f8783eb7b73ddd0c90
Helina Alex <helinatalex@gmail.com>
2025-07-27 11:11:46 +0530

    add reuirements.txt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
 export default defineConfig({
+  baseURL: import.meta.env.VITE_API_BASE_URL
   server: {
     port: 5173,
     host: "localhost",
